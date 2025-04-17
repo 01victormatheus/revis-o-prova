1237b370a79638c63b6e9134727b62587d625a08
victor matheus <vtinnmatheus@gmail.com>
2025-04-17 09:37:33 -0300

    atividade dos sucessor e antecessor do numero

--- exercicio 2.py
+++ exercicio 2.py
@@ -1,15 +1,18 @@
-num=int(input("digite um número a partir de 1"))
-divisao=num % 2
+repetir="sim"
+while repetir =="sim":
+    num = int(input("digite um número a partir de 1"))
+    divisao = num % 2
 
-if num >0:
-    if divisao ==0:
-        print("este numero é par positivo")
+    if num > 0:
+        if divisao == 0:
+            print("este numero é par positivo")
+        else:
+            print("este número é impar positivo")
     else:
-        print("este número é impar positivo")
-else:
-    if divisao ==0:
-        print("este numero é par negativo")
-    else:
-        print("este número é impar negativo")
+        if divisao == 0:
+            print("este numero é par negativo")
+        else:
+            print("este número é impar negativo")
 
 
+    repetir=input("quer coninuar verificando?")
---
+++ exercicio 3.py
@@ -0,0 +1,4 @@
+num=int(input("digite um numero"))
+numa=num-1
+nums=num+1
+print(f"o antecessor é:{numa},o numero é {num},o sucessor é :{nums}")
