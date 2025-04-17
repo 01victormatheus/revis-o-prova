7b177b55be70c41261e30951ccd64a7c27fd43b3
victor matheus <vtinnmatheus@gmail.com>
2025-04-17 08:34:44 -0300

    exercicio dos valores a,b,c

---
+++ exercicio 1.py
@@ -0,0 +1,11 @@
+valorA=int(input("digite um valor"))
+valorB=int(input("digite um valor"))
+valorC=int(input("digite um valor"))
+
+soma=valorA+valorB
+
+if soma < valorC:
+    print(f" a soma de valor a  e valor b {soma},é menor que valor c")
+
+else:
+    print(f"a soma foi maior do que c")
