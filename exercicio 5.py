8b6ddb14894b990e90e72ddb0a6f0c7f2c37021a
victor matheus <vtinnmatheus@gmail.com>
2025-04-17 11:06:34 -0300

    atividade imc

--- exercicio 5.py
+++ exercicio 5.py
@@ -2,15 +2,15 @@ peso=float(input("digite seu peso"))
 altura=float(input("digite sua altura"))
 imc=peso/(altura*altura)
 print(imc)
-if imc < 18.5:
+if imc < 18.6:
     print("abaixo do peso")
 elif imc >=18.6 and imc <=24.9:
     print("peso ideal,parabéns")
-elif imc == 25.0 and imc <=29.9:
+elif imc >= 25.0 and imc <=29.9:
     print("levemente acima do peso")
-elif imc == 30.0 and imc <=34.9:
+elif imc >= 30.0 and imc <=34.9:
     print("obesidade grau 1")
-elif imc == 35.0 and imc <=39.9 :
+elif imc >= 35.0 and imc <=39.9 :
     print("obesidade grau 2,severa")
 elif imc >=40:
     print("obesidade grau 3,morbida")
